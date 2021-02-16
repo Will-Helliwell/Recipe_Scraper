e86807aa37fb9f77e5e52525ce1367e37da14945
AJ <aj.monty@gmail.com>
2021-02-16 14:08:52 +0000

    tampering

--- recipeScraper.js
+++ recipeScraper.js
@@ -4,12 +4,12 @@ const recipeScraper = require("recipe-scraper");
 // enter a supported recipe url as a parameter - returns a promise
 async function someAsyncFunc() {
 //   ...
-  let recipe = await recipeScraper("https://www.bbc.co.uk/food/recipes/creamy_chicken_ham_and_03877");
+  let recipe = await recipeScraper("https://www.bbcgoodfood.com/recipes/coconut-pastries-gujiya");
 //   ...
 }
 
 // using Promise chaining
-recipeScraper("https://www.bbc.co.uk/food/recipes/creamy_chicken_ham_and_03877").then(recipe => {
+recipeScraper("https://www.bbcgoodfood.com/recipes/coconut-pastries-gujiya").then(recipe => {
     console.log(recipe)
     // do something with recipe
   }).catch(error => {
