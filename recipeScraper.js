e356b2fe2e75261eec0389e40c4d4d331389d427
AJ <aj.monty@gmail.com>
2021-02-17 17:31:10 +0000

    added urls.js

--- recipeScraper.js
+++ recipeScraper.js
@@ -1,15 +1,18 @@
 // import the module
 const recipeScraper = require("recipe-scraper");
+require("urls")
+
+console.log(urls)
 
 // enter a supported recipe url as a parameter - returns a promise
 async function someAsyncFunc() {
 //   ...
-  let recipe = await recipeScraper("https://www.bbcgoodfood.com/recipes/coconut-pastries-gujiya");
+  let recipe = await recipeScraper("https://www.bbcgoodfood.com/recipes/carrot-parsnip-soup");
 //   ...
 }
 
 // using Promise chaining
-recipeScraper("https://www.bbcgoodfood.com/recipes/coconut-pastries-gujiya").then(recipe => {
+recipeScraper("https://www.bbcgoodfood.com/recipes/carrot-parsnip-soup").then(recipe => {
     console.log(recipe)
     // do something with recipe
   }).catch(error => {
