4774ff2b6d28093af71e1be72ad4d5e4f8cd60df
AJ <aj.monty@gmail.com>
2021-02-18 16:20:47 +0000

    trying to get fetch working

--- recipeScraper.js
+++ recipeScraper.js
@@ -11,22 +11,37 @@ const urls = require("./urls")
 // //   ...
 // }
 
-urls.forEach(url => {
-  recipeScraper(url).then(recipe => {
-    console.log(recipe)
-    // do something with recipe
-  }).catch(error => {
-    // do something with error
-  });
-})
-
-
-
-
-// using Promise chaining
-// recipeScraper("https://www.bbcgoodfood.com/recipes/easy-pancakes").then(recipe => {
+// urls.forEach(url => {
+//   recipeScraper(url).then(recipe => {
 //     console.log(recipe)
 //     // do something with recipe
 //   }).catch(error => {
 //     // do something with error
 //   });
+// })
+
+
+
+
+// using Promise chaining
+recipeScraper("https://www.bbcgoodfood.com/recipes/easy-pancakes").then(recipe => {
+    console.log(JSON.stringify(recipe))
+    fetch('https://localhost:5000/api/todos', {
+    method: 'POST', // or 'PUT'
+    headers: {
+    'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(recipe),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+    // do something with recipe
+  }).catch(error => {
+    // do something with error
+  });
